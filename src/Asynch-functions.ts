dbe17ba8502022e4f5ecc7c18745d664f5c142a2
wamurombedzi <wadzamurombedzi@gmail.com>
2026-09-22 01:42:05 +0300

    functions done

---
+++ src/Asynch-functions.ts
@@ -0,0 +1,24 @@
+console.log(1);
+
+setTimeout(() => {
+    console.log('waited 1 sec')
+}, 1000);
+
+console.log(2)
+
+function getEmployeeData() {
+    const employeeData = {
+        name: 'Wadza',
+        position: 'Software Engineer',
+        age: 30,
+        tasks: ['Write code', 'Drink coffee', 'Write programming cliches']
+    }
+    return Promise.resolve(employeeData)
+}
+
+let employeeData: any;
+getEmployeeData().then(data => {
+    employeeData = data
+}).then(() => {
+    console.log(employeeData)
+})
